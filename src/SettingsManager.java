fcaf541fc363334986e4dfd206f8adc315d92e1c
Karlos <mdmrrr.34@gmail.com>
2023-04-24 19:22:14 +0300

    Settings Manager artik muthis, anime kizi guncellemesi

--- src/SettingsManager.java
+++ src/SettingsManager.java
@@ -15,6 +15,14 @@ import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import javafx.scene.text.*;
+import javafx.scene.control.ComboBox;
+import javafx.collections.FXCollections;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.Button;
+
+
+
+
 
 public class SettingsManager extends Application{
     public static void main(String[] args) {
@@ -81,25 +89,86 @@ public class SettingsManager extends Application{
                     public void changed(ObservableValue <? extends Number >
                                                 observable, Number oldValue, Number newValue)
                     {
-
-                        System.out.println(newValue.intValue());
                     }
                 });
+
+        Text opacityText = new Text();
+        opacityText.setFont(new Font(20));
+        opacityText.setTextAlignment(TextAlignment.CENTER);
+        opacityText.setText("Parlaklık");
+
+        Slider slider2 = new Slider(0, 100, 0);
+        slider2.setBlockIncrement(10);
+        slider2.setMajorTickUnit(10);
+        slider2.setMinorTickCount(0);
+        slider2.setShowTickLabels(true);
+        slider2.setSnapToTicks(true);
+
+        System.out.println(slider2.getValue());
+        slider2.valueProperty().addListener(
+                new ChangeListener<Number>() {
+                    public void changed(ObservableValue <? extends Number >
+                                                observable, Number oldValue, Number slider2NewValue)
+                    {
+                    }
+                });
+
+        Text resText = new Text();
+        resText.setFont(new Font(20));
+        resText.setTextAlignment(TextAlignment.CENTER);
+        resText.setText("Çözünürlük");
+
+        String week_days[] =
+                {"800x600", "1280x720", "1920x1080"};
+
+        // Create a combo box
+        ComboBox combo_box =
+                new ComboBox(FXCollections
+                        .observableArrayList(week_days));
+
+
+        CheckBox cb1 = new CheckBox();
+//A checkbox with a string caption
+        cb1.setText("Tam Ekran");
+        cb1.setSelected(true);
+        //boolean isSelected = checkBox1.isSelected();
+        cb1.setFont(Font.font("Arial", 17));
+
+
 
         Image backgroundImage = new Image("images/settingswp.jpg");
         ImageView backgroundImageView = new ImageView(backgroundImage);
         StackPane stackPane = new StackPane();
         // Scene ve Stage ayarları
         Scene scene = new Scene(stackPane, 350, 500);
+
         VBox vbox1 = new VBox();
         vbox1.setPadding(new Insets(0, 0, 0, 0));
-        vbox1.getChildren().addAll(audioText, slider);
+        vbox1.getChildren().addAll(audioText, slider, opacityText, slider2, resText, combo_box);
         vbox1.setSpacing(10);
         vbox1.setAlignment(Pos.TOP_CENTER);
+
         backgroundImageView.fitWidthProperty().bind(scene.widthProperty());
         backgroundImageView.fitHeightProperty().bind(scene.heightProperty());
         backgroundImageView.setOpacity(0.5);
-        stackPane.getChildren().addAll(backgroundImageView, vbox1);
+        Button kaydet = new Button("Kaydet");
+        kaydet.setFont(Font.font("Arial", FontWeight.BOLD, 15));
+        Button menuyedon = new Button("Menüye Dön");
+        menuyedon.setFont(Font.font("Arial", FontWeight.BOLD, 15));
+
+        Image anime = new Image("images/setimage.png");
+        ImageView animeView = new ImageView(anime);
+        animeView.setFitHeight(200);
+        animeView.setFitWidth(280);
+
+
+        stackPane.getChildren().addAll(backgroundImageView, vbox1, cb1,animeView,kaydet,menuyedon);
+        StackPane.setAlignment(cb1, Pos.CENTER_LEFT);
+        StackPane.setAlignment(kaydet, Pos.BOTTOM_LEFT);
+        StackPane.setAlignment(menuyedon, Pos.BOTTOM_RIGHT);
+        StackPane.setMargin(animeView, new Insets(300, 0, 0, -20));
+
+
         primaryStage.setTitle("Settings Manager");
         primaryStage.setResizable(false);
         primaryStage.setScene(scene);
