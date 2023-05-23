c91823f87f903a8eac409997b6337698c1209a9e
cniex <abdulkadircan03@gmail.com>
2023-05-23 19:12:49 +0300

    sahne geçişi

--- src/Game.java
+++ src/Game.java
@@ -1,9 +1,17 @@
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+import javafx.scene.media.MediaView;
 import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
+
+import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 import java.io.FileInputStream;
@@ -11,6 +19,10 @@ import java.io.IOException;
 import java.util.Properties;
 //
 public class Game extends Application {
+
+    Stage window;
+    Scene scene1, scene2;
+
     public static void main(String[] args) {
         SettingsManager settingsManager = new SettingsManager();
         settingsManager.checkifSettings(); //once ayarlarin olup olmadigi kontrol edilecek
@@ -20,6 +32,8 @@ public class Game extends Application {
     private static final String FILE_PATH = "settings.conf"; // -> settings.conf production asamasinda out/production/ClickGame icerisinde, oyunun ismi degisicek :D
     @Override
     public void start(Stage primaryStage) {
+        window = primaryStage;
+
         SettingsManager settingsMng = new SettingsManager();
         int intAudio = settingsMng.returnAudioValue();
 
@@ -38,13 +52,31 @@ public class Game extends Application {
         String fullScreen= settingsMng.returnScreenValue();
         boolean isFullScreen=Boolean.parseBoolean(fullScreen);
 
-        StackPane stackPane = new StackPane();
+        Label label1 = new Label("yazı yazmaca");
+        Button button1 = new Button("İntroyu geç");
+        //layout1
+        VBox layout1 = new VBox(20);
+        layout1.getChildren().addAll(label1, button1);
+        scene1 = new Scene(layout1, 600,300);
+        //layout 1 ve scene 1 bitiş
 
-        Scene scene = new Scene(stackPane, width, height);
-        primaryStage.setTitle("Beyond The Veil");
-        primaryStage.setScene(scene);
-        primaryStage.setOpacity(opacity);
-        primaryStage.setFullScreen(isFullScreen);
-        primaryStage.show();
+        //layout2
+        Button button2 = new Button("geri dön");
+        button2.setOnAction(e -> window.getScene().setRoot(layout1));
+
+        StackPane layout2 = new StackPane();
+        layout2.getChildren().add(button2);
+        //layout2 bitiş
+
+        button1.setOnAction(e -> window.getScene().setRoot(layout2));
+
+
+
+
+        window.setTitle("Beyond The Veil");
+        window.setScene(scene1);
+        window.setOpacity(opacity);
+        window.setFullScreen(isFullScreen);
+        window.show();
     }
 }
